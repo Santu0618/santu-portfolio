e24acb678cec5170195a89842380e88578ee9c72
Santu0618 <kavalasanthosh00@gmail.com>
2025-02-20 14:25:49 +0530

    Deploy React app to GitHub Pages

--- santu-portfolio/src/index.js
+++ santu-portfolio/src/index.js
@@ -7,6 +7,7 @@ import Navbar from './components/Navbar';
 import Home from './components/Home';
 import Footer from './components/Footer';
 import Experience from './components/Experience';
+import { HashRouter as Router } from "react-router-dom";
 import About from './components/About';
 import Contact from './components/Contact';
 import Projects from './components/Projects';
